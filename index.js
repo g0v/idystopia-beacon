3fb91a11f5fdc52963cd8a5ade7ef710f92bfd3e
YuTin <yuting1987@gmail.com>
2020-12-19 12:58:09 +0800

    feat(scan): added scan decode

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const noble = require('@abandonware/noble');
 const bleno = require('./index');
 const terminalImage = require('terminal-image');
 
+const EXPECTED_MANUFACTURER_DATA_LENGTH = 25;
+const APPLE_COMPANY_IDENTIFIER = 0x004c; // https://www.bluetooth.org/en-us/specification/assigned-numbers/company-identifiers
+const IBEACON_TYPE = 0x02;
+const EXPECTED_IBEACON_DATA_LENGTH = 0x15;
+
 noble.on('stateChange', function(state) {
   console.log('on -> stateChange: ' + state);
   if (state === 'poweredOn') {
@@ -12,7 +17,45 @@ noble.on('stateChange', function(state) {
 });
 
 noble.on('discover', (peripheral) => {
-  console.log(peripheral);
+  const manufacturerData = peripheral.advertisement.manufacturerData;
+  const rssi = peripheral.rssi;
+
+  if (manufacturerData &&
+      EXPECTED_MANUFACTURER_DATA_LENGTH <= manufacturerData.length &&
+      APPLE_COMPANY_IDENTIFIER === manufacturerData.readUInt16LE(0) &&
+      IBEACON_TYPE === manufacturerData.readUInt8(2) &&
+      EXPECTED_IBEACON_DATA_LENGTH === manufacturerData.readUInt8(3)) {
+
+    const uuid = manufacturerData.slice(4, 20).toString('hex');
+    const major = manufacturerData.readUInt16BE(20);
+    const minor = manufacturerData.readUInt16BE(22);
+    const measuredPower = manufacturerData.readInt8(24);
+
+    const accuracy = Math.pow(12.0, 1.5 * ((rssi / measuredPower) - 1));
+
+    let proximity = null;
+    if (accuracy < 0) {
+      proximity = 'unknown';
+    } else if (accuracy < 0.5) {
+      proximity = 'immediate';
+    } else if (accuracy < 4.0) {
+      proximity = 'near';
+    } else {
+      proximity = 'far';
+    }
+
+    const bleacon = {};
+
+    bleacon.uuid = uuid;
+    bleacon.major = major;
+    bleacon.minor = minor;
+    bleacon.measuredPower = measuredPower;
+    bleacon.rssi = rssi;
+    bleacon.accuracy = accuracy;
+    bleacon.proximity = proximity;
+
+    console.log(bleacon);
+  }
 });
 
 console.clear();
