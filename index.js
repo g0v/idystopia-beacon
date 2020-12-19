a1461b8121653e807496b12b5af95074dac956ff
YuTin <yuting1987@gmail.com>
2020-12-19 12:40:45 +0800

    feat(scan): added console

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bleno = require('./index');
 const terminalImage = require('terminal-image');
 
 noble.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
   if (state === 'poweredOn') {
     noble.startScanning(['d88815463ebd11eba0431f17d6b26b6b'], true);
   } else {
